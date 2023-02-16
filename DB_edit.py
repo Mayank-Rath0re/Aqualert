2aaea3b21b07909c9a9202d5e8c9287c2517da46
Mayank-Rath0re <mayankrathore2018@gmail.com>
2023-02-16 14:42:35 +0530

    Structured DB_edit.py

--- DB_edit.py
+++ DB_edit.py
@@ -1,3 +1,22 @@
 import mysql.connector
-db = mysql.connector.connect(host="localhost",user="root",password="root")
+db = mysql.connector.connect(host="localhost",user="root",password="root",database="aqualert")
 cursor=db.cursor()
+def inp_data():
+    pass
+def rollback():
+    pass
+def main():
+    while True:
+        print("1. Enter Data")
+        print("2. Rollback Last Function")
+        print("3. Exit")
+        ask = int(input("Enter S.No. Of Function: "))
+        if ask==1:
+            inp_data()
+        elif ask==2:
+            rollback()
+        elif ask==3:
+            break
+        else:
+            print("Invalid Input")
+main()
