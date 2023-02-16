a8eddd5d7ad49de8648055bf02108ac23e8ed731
Mayank-Rath0re <mayankrathore2018@gmail.com>
2023-02-16 17:02:05 +0530

    Started Working on coordinates

---
+++ csv_file.py
@@ -0,0 +1,36 @@
+import csv
+datalist=[]
+def store():
+    file = open("coordinates.csv")
+    csv_reader = csv.reader(file)
+    for row in csv_reader:
+        if row == []:
+            pass
+        else:
+            datalist.append(row)
+    file.close()
+def restore():
+    file = open("coordinates.csv", 'w')
+    csv_writer = csv.writer(file)
+    csv_writer.writerows(datalist)
+    file.close()
+def main():
+    store()
+    try:
+        while True:
+            print("1. Add Data")
+            print("2. Exit")
+            ask = int(input("Enter Choice: "))
+            if ask==1:
+                name=input("Enter Name: ")
+                Latitude1=float(input("Latitude 1: "))
+                Latitude2=float(input("Latitude 2: "))
+                Longitude1=float(input("Longitude 1: "))
+                Longitude2=float(input("Longitude 2: "))
+                datalist.append([name,Latitude1,Latitude2,Longitude1,Longitude2])
+            else:
+                break
+    except:
+        restore()
+    restore()
+main()
